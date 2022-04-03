86435253f1e98559e59766c7ab41e955508d9812
Your name <tylershifflett@Tylers-MacBook-Pro.local>
2022-04-03 18:07:36 -0400

    Add Javascript

--- javascript.js
+++ javascript.js
@@ -13,5 +13,28 @@ var printTasks = function() {
     $.each(tasks, function(list, arr) {
         var taskP = $("<p>").addClass("description task-item-" + list).text(arr)
         console.log(list)
+    })
+}
+var Today = (moment().format("MMMM D, YYYY"))
+    $("#currentDay").text(Today);
+
+var hourAudit = function (){
+    var currentHour = moment().hour()
+    for(var i=8; i<18; i++){
+        var taskArea = $("#task-"+i)
+        if(currentHour>i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        } else{
+            $(taskArea).addClass("future")
+        }
     }
 }
+
+
+$(".saveBtn").on("click", function() {
+    var index = $(".saveBtn").index(this);
+    task[index] = $(this).parent().find(",taskItem").text();
+    localstore.setItem("tasks", JSON.stringify(tasks));
+});
